81e2c21a567b0e2a164880a74761c2b4b9a3c2af
Leonwei-cyb <Leonweizhong@gmail.com>
2024-10-30 12:13:55 -0600

    add pan effeect

--- Perlin Noise/sketch.js
+++ Perlin Noise/sketch.js
@@ -5,7 +5,8 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 let rectWidth = 1;
-
+let speed = 0.01;
+let xStart = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -14,23 +15,38 @@ function setup() {
 function draw() {
   background(255);
   staircase();
+
+  xStart += speed;
 }
 
 
 function staircase(){
+  let xpan = xStart;
+  let highestpeak = 0;
+  let heightloc = 0;
+  let averageheight = 0;
   //use a for loop to draw a series of 
   //rectangles for use as terrain
-  randomSeed(1); //only for random()
+  //randomSeed(1); //only for random()
   
   for(let x = 0; x <= width; x += rectWidth){
     noFill();
     //generate a random height for each rect
-    let rectHeight = noise(x/100);
+    let rectHeight = noise(xpan);
     
     rectHeight = map(rectHeight, 0, 1, 500, 100)
 
     rect(x, height, rectWidth, -rectHeight);
+    xpan += 0.01
+    if (rectHeight > highestpeak){
+      
+
+    }
   }
+  let averageheight = highestpeak/count;
+    stroke()
+    fill("red")
+    rect(30, 20, 55, 40)
 }
 
 //
@@ -42,6 +58,10 @@ function keyPressed() {
   }
   return
 }
-function drawFlag(x,y) {
-  
+function drawFlag() {
+  stroke()
+  line(x, y, x, y, x, y)
+  fill(0,0,0);
+  noStroke();
+  triangle(x, y, x, y, x, y);
 }
